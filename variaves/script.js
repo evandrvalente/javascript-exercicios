1bb833317111dd253458352bf8cedb64eb50dde6
Evandro Valente <evandr.valente@gmail.com>
2024-10-14 17:59:19 -0300

    update variaveis

---
+++ variaves/script.js
@@ -0,0 +1,29 @@
+// isso é um comentário
+
+// tipos primitivos
+
+// number 
+let pi = 3.1415
+console.log(`A variável ${pi} é do tipo ${typeof(pi)}`)
+
+// string
+let nome = 'Pafúncio'
+console.log(`A variável ${nome} é do tipo ${typeof(nome)}`)
+
+// boolean
+let corinthiansrebaixado = "7"
+console.log(`A variável ${corinthiansrebaixado} é do tipo ${typeof(corinthiansrebaixado)}`)
+
+// o amor é uma flor roxa
+let frutas = ["pera", "uva","maçã"]
+console.log(`A variável ${frutas} é do tipo ${typeof(frutas)}`)
+
+function xablau(){
+  console.log("oi sou a funçao xablau")
+};
+
+xablau()
+
+let vamoquevamo = function(){}
+console.log(`A variável ${vamoquevamo} é do tipo ${typeof(vamoquevamo)}`)
+
